699d5b1c50824c39494fe3a32a85b36d2f856ecb
Yahya305 <saimyahya47@gmail.com>
2023-01-26 21:08:50 +0500

    Done some routing
    There is still cancel button error and a new navbar showing up problem in routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import React from 'react'
 import NavBar from './components/NavBar';
 import Home from './components/Home'
 import Notifications from './components/Notifications'
-import Profile from './components/SearchBox'
 import Contact from './components/Contact'
 import About from './components/About'
 import {
@@ -14,29 +11,29 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import {useState} from 'react';
+// import {useState} from 'react';
 
 // used rfce
 function App() {
-  const [mode, setMode] = useState('light');
-
-  const toggleButton= ()=>{
-    // console.log(mode);
-    if (mode==='light') {
-      setMode('dark')
-    } else {
-      setMode('light')      
-    }
-  }
+  // const [mode, setMode] = useState('light');
+  // const toggleButton= ()=>{
+  //   // console.log(mode);
+  //   if (mode==='light') {
+  //     setMode('dark')
+  //   } else {
+  //     setMode('light')      
+  //   }
+  // }
   return (
     <BrowserRouter>
       <NavBar></NavBar>
       <Routes>
-        <Route path='/' element={<Home mode={mode}/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/' element={<Home/>}/>
+        {/* <Route path='/profile' element={<Profile/>}/> */}
         <Route path='/notifications' element={<Notifications/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
+        <Route path="*" element={<div><center>Page Not Found</center></div>} />
       </Routes>
     </BrowserRouter>
     
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import ImageContainer from './ImageContainer'
+import SearchBar from './SearchBar'
 
 function Home(props) {
+  const [data, setdata] = useState({});
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/quotes").then(
+      res => res.json()
+    ).then(
+      f => setdata(f)
+    )
+  },[]);
+
+  // usestate for setting a javascript
+    // object for storing and using data
+    // let quote;
+    // let p=fetch("http://localhost:5000/data");
+    // p.then(
+    //   res => res.json()
+    // ).then(
+    //   // data=>setdata(data)
+    //   quote=data
+    // )
+    // console.log(quote)
+  // Using useEffect for single rendering
+  // http://localhost:5000/data
   
   return (
     <>
+    <div className='navcontent'>
+      <span className='navquote'>
+        <h1 id='title'>The Notebook</h1>
+        <div><h4 className='quote'>{data.quote}</h4></div>
+        <div className='author'>---By {data.author}</div>
+        <SearchBar></SearchBar>
+      </span>
+      <img className='navimg' src='navbar image2.jpg' alt='unavailable'></img>
+    </div>
       <ImageContainer/>
       
     </>
